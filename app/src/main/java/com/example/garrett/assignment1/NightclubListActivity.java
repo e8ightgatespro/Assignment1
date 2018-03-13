eebbaaf420c2a8b05f061c690fa458f45befe71f
Garrett Voss <garrettvoss4@gmail.com>
2018-03-13 18:55:24 -0500

    Completed NIghtclubListActivity

--- app/src/main/java/com/example/garrett/assignment1/NightclubListActivity.java
+++ app/src/main/java/com/example/garrett/assignment1/NightclubListActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class NightclubListActivity extends AppCompatActivity {
+
+    boolean isDeleteing = false;
+    NightclubAdapter adapter;
+    ArrayList<Nightclub> nightclubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,45 @@ public class NightclubListActivity extends AppCompatActivity {
         initAddButton();
         initMapButton();
         initListButton();
+        initItemClick();
+        initDeleteButton();
+    }
+
+    private void initDeleteButton() {
+        final Button deleteButton = findViewById(R.id.buttonDeleteNightclub);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isDeleteing) {
+                    deleteButton.setText("Delete");
+                    isDeleteing = false;
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    deleteButton.setText("Done Deleting");
+                    isDeleteing = true;
+                }
+            }
+        });
+    }
+
+    private void initItemClick() {
+        ListView listView = findViewById(R.id.lvNightclubs);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+                Nightclub selectedNightclub = nightclubs.get(position);
+                if(isDeleteing) {
+                    adapter.showDelete(position, itemClicked, NightclubListActivity.this, selectedNightclub);
+                }
+                else {
+                    Intent intent = new Intent(NightclubListActivity.this, NightclubRatingActivity.class);
+                    intent.putExtra("nightclubid", selectedNightclub.getNightclubID());
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 
 
