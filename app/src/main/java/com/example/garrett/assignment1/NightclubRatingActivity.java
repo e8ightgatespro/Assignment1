cc592e11ae254a1f5603d6d156ebd79a67fa5123
Garrett Voss <garrettvoss4@gmail.com>
2018-03-13 19:11:30 -0500

    Began NihgtlcubRatingActivity

--- app/src/main/java/com/example/garrett/assignment1/NightclubRatingActivity.java
+++ app/src/main/java/com/example/garrett/assignment1/NightclubRatingActivity.java
@@ -1,8 +1,54 @@
 package com.example.garrett.assignment1;
 
-/**
- * Created by Garrett on 3/12/2018.
- */
 
-public class NightclubRatingActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NightclubRatingActivity extends AppCompatActivity {
+    private Nightclub currentNightclub;
+
+    @Override
+    protected void OnCreate(Bundle savedInstanceStates) {
+        super.onCreate(savedInstanceStates);
+        setContentView(R.layout.activity_nightclub_rating);
+        initAdd();
+        initList();
+        initMap();
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            initNightclub(extras.getInt("nightclubid"));
+        }
+
+
+    }
+
+    private void initNightclub(int id) {
+        NightclubDataSource ds = new NightclubDataSource(NightclubRatingActivity.this);
+        try {
+            ds.open();
+            currentNightclub = ds.getSpecificNightclub(id);
+            ds.close();
+        }
+        catch(Exception e) {
+            Toast.makeText(this, "Load Nightclub Failed", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
+
+    private void initAdd() {
+
+    }
+
+    private void initList() {
+
+    }
+
+    private void initMap() {
+
+    }
 }
