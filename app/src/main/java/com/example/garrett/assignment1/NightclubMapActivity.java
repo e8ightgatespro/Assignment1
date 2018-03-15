fdd1607c8388b027edec8a01394fcc402b6f4cab
Garrett Voss <garrettvoss4@gmail.com>
2018-03-14 22:18:24 -0500

    Almost done with map implementation

--- app/src/main/java/com/example/garrett/assignment1/NightclubMapActivity.java
+++ app/src/main/java/com/example/garrett/assignment1/NightclubMapActivity.java
@@ -1,11 +1,13 @@
 package com.example.garrett.assignment1;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
-import android.support.design.widget.Snackbar;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -13,6 +15,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
