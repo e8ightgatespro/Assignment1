02e723220187980dec3c7a69d9755654e9804163
Garrett Voss <garrettvoss4@gmail.com>
2018-03-14 21:56:28 -0500

    Almost done with map implementation

--- app/src/main/java/com/example/garrett/assignment1/NightclubMapActivity.java
+++ app/src/main/java/com/example/garrett/assignment1/NightclubMapActivity.java
@@ -1,27 +1,79 @@
 package com.example.garrett.assignment1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.support.design.widget.Snackbar;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class NightclubMapActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NightclubMapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, com.google.android.gms.location.LocationListener {
+    final int PERMISSION_REQUEST_LOCATION = 101;
+    GoogleMap gMap;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    ArrayList<Nightclub> nightclubs = new ArrayList<>();
+    Nightclub currentNightclub = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nightclub);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            NightclubDataSource ds = new NightclubDataSource(NightclubMapActivity.this);
+            ds.open();
+            if (extras != null) {
+                currentNightclub = ds.getSpecificNightclub(extras.getInt("contactid"));
+            } else {
+                nightclubs = ds.getNightclubs();
+            }
+            ds.close();
+        } catch (Exception e) {
+            Toast.makeText(this, "Contact(s) could not be retrieved.", Toast.LENGTH_LONG).show();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        createLocationRequest();
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        }
+
         initAddButton();
         initMapButton();
         initListButton();
-        initSaveButton();
-
-    }
-
-    private void initSaveButton() {
-        Button saveButton = (Button) findViewById(R.id.buttonSaveClub);
 
     }
 
@@ -61,4 +113,191 @@ public class NightclubMapActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void startLocationUpdates() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        gMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case PERMISSION_REQUEST_LOCATION: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
+                }
+                else {
+                    Toast.makeText(NightclubMapActivity.this,"Assignment 1 will not locate your clubs.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    protected void OnStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void OnStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    protected void createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                gMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+
+        gMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                gMap.addMarker(new MarkerOptions().position(latLng).title("I'm Here"));
+            }
+        });
+
+        Point size = new Point();
+        WindowManager w = getWindowManager();
+        w.getDefaultDisplay().getSize(size);
+        int measureWidth = size.x;
+        int measureHeight = size.y;
+
+        if(nightclubs.size() > 0) {
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            for (int i = 0; i<nightclubs.size(); i++) {
+                currentNightclub = nightclubs.get(i);
+                Geocoder geo = new Geocoder(this);
+                List<Address> addresses = null;
+
+                String address = currentNightclub.getStreetaddress() + ", " +
+                        currentNightclub.getCity() + ", " +
+                        currentNightclub.getState() + ", " +
+                        currentNightclub.getZipcode();
+                try {
+                    addresses = geo.getFromLocationName(address, 1);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                LatLng point = new LatLng(addresses.get(0).getLatitude(),addresses.get(0).getLongitude());
+                builder.include(point);
+
+                gMap.addMarker(new MarkerOptions().position(point).title(currentNightclub.getNightclubName()).snippet(address));
+            }
+            gMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), measureWidth, measureHeight, 450));
+        }
+        else {
+            if (currentNightclub != null) {
+                Geocoder geo = new Geocoder(this);
+                List<Address> addresses = null;
+
+                String address = currentNightclub.getStreetaddress() + ", " +
+                        currentNightclub.getCity() + ", " +
+                        currentNightclub.getState() + ", " +
+                        currentNightclub.getZipcode();
+
+                try {
+                    addresses = geo.getFromLocationName(address, 1);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                LatLng point = new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
+
+                gMap.addMarker(new MarkerOptions().position(point).title(currentNightclub.getNightclubName()).snippet(address));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(point, 16));
+            }
+            else {
+                AlertDialog alertDialog = new AlertDialog.Builder(NightclubMapActivity.this).create();
+                alertDialog.setTitle("No Data");
+                alertDialog.setMessage("No data is available for the mapping function");
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                alertDialog.show();
+            }
+        }
+
+        try {
+            if(Build.VERSION.SDK_INT >= 23) {
+                if(ContextCompat.checkSelfPermission(NightclubMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(NightclubMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        Snackbar.make(findViewById(R.id.activity_nightclub),"Assignment 1 requires this permission to locate " + "your clubs", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(NightclubMapActivity.this, new String[]{
+                                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+                            }
+                        }).show();
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(NightclubMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+                    }
+                }
+                else {
+                    startLocationUpdates();
+                }
+
+            }
+            else {
+                startLocationUpdates();
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(getBaseContext(), "Error requesting permission", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getBaseContext(), "Lat: " + location.getLatitude() + " Long: " + location.getLongitude() + " Accuracy: " + location.getAccuracy(),Toast.LENGTH_LONG).show();
+    }
+
 }
